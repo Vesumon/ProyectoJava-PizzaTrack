63cd035c5c191b9f4a4f264ccd5522984cf3e7d9
Vesumon <proyeremy12349@gmail.com>
2026-03-15 12:02:25 -0500

    Implemetanción de la pila manual con listas ligadas al ProyectoPizzaTrack

--- src/Pila.java
+++ src/Pila.java
@@ -1,3 +1,36 @@
+// Implementación manual de una pila usando lista ligada
 public class Pila {
     
+    //Puntero al nodo que está en el tope
+    Nodo tope;
+
+    public Pila(){
+        tope = null;
+    }
+
+    //Verifica si la pila está vacía
+    public boolean isEmpty(){
+        return tope == null;
+    }
+
+    //Inserta una pizza en el tope de la pila
+    public void push(Pizza pizza){
+        Nodo nuevo = new Nodo(pizza);
+
+        //El nuevo nodo apunta al antiguo tope
+        nuevo.siguiente = tope;
+
+        //El tope ahora es el nuevo nodo
+        tope = nuevo;
+    }
+
+    //Elimina el elemento del tope
+    public Pizza pop(){
+
+        if(isEmpty()){
+            return null;
+        }
+
+        return tope.pizza;
+    }
 }
